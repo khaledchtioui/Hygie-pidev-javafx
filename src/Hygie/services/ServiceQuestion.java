a36c6e77695f55e6723b8cfd9d82a145bb6e0820
khaledchtioui <khaled.chtioui@esprit.tn>
2023-03-30 10:39:26 +0100

    Crud

---
+++ src/Hygie/services/ServiceQuestion.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hygie.services;
+
+import Hygie.entities.Questionnaire;
+import Hygie.entities.Questions;
+import Hygie.utils.MyDB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Khaled
+ */
+public class ServiceQuestion implements QuizService<Questions>{
+
+    
+      
+    Connection con ; 
+    Statement ste;
+     
+    
+    
+    
+    
+    
+    public ServiceQuestion() {
+        
+        con = MyDB.createorgetInstance().getCon();
+        
+    }
+
+    
+    
+    @Override
+    public void Ajouter(Questions t) {
+
+   try {
+       
+       
+       
+            
+            //1 creer le statement 
+            ste = con.createStatement();
+           int idQuiz=t.getQuizid().getId() ;
+           System.out.println(idQuiz);
+            
+            String req = "INSERT INTO `pppppp`.`Questions` (`question`,`quizid_id`,`type`,`point`) VALUES ('"+t.getQuestion()+"','"+idQuiz+"','"+t.getType()+"','"+t.getPoint()+"');";
+
+            
+            ste.executeUpdate(req);
+            
+            
+        }  catch (SQLException ex) {
+            System.err.println("Questions exception");
+            System.out.println(ex.getMessage());
+        }
+        
+
+
+    }
+
+
+    @Override
+    public void supprimer(Questions t) {
+
+
+
+        //
+            String sql = "delete from Questions where id =?";
+        try {
+            PreparedStatement ste = con.prepareStatement(sql);
+            ste.setInt(1, t.getId());
+            ste.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        //
+        
+
+    }
+
+    public  List<Map<String, Object>> getAll2() {
+
+
+
+
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        try {
+
+        ResultSet rs = ste.executeQuery("SELECT questionnaire.id AS questionnaire_id, questionnaire.nom AS questionnaire_nom, questionnaire.date AS questionnaire_date, questions.id AS question_id, questions.question AS question_question, questions.type AS question_type, questions.point AS question_point FROM questionnaire JOIN questions ON questionnaire.id = questions.quizid_id ")  ;
+    while (rs.next()) {
+        Map<String, Object> row = new HashMap<>();
+        row.put("questionnaire_id", rs.getInt("questionnaire_id"));
+        row.put("questionnaire_nom", rs.getString("questionnaire_nom"));
+     // row.put("questionnaire_date", rs.getObject("questionnaire_date", LocalDateTime.class));
+        row.put("question_id", rs.getInt("question_id"));
+        row.put("question_question", rs.getString("question_question"));
+        row.put("question_type", rs.getInt("question_type"));
+        row.put("question_point", rs.getInt("question_point"));
+        resultList.add(row);
+    }
+    
+     } catch (SQLException ex) {
+            System.err.println("errr");
+            System.out.println(ex.getMessage());
+        }
+        
+
+
+
+return resultList    ;
+    }
+
+    
+    
+        public void modifier(String question,int type,int point ,Questions t) 
+            {
+                   String sql = "update Questions set question=? ,type=?,point=? where id=?";
+
+        try {
+            PreparedStatement ste = con.prepareStatement(sql);
+            ste.setString(1, question);
+            ste.setInt(2,type);
+            ste.setInt(3,point);
+            ste.setInt(4,t.getId())  ;
+            ste.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+                
+            }
+
+    
+    
+    @Override
+    public List<Questions> getAll() {
+        System.err.println("err");   return null;
+ }
+    
+}
